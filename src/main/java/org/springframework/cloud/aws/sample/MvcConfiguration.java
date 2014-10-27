59e49e8501a396fd16f3a4df61dfd826fcfb3a8d
Alain Sahli <sahli.alain@gmail.com>
2014-10-27 11:49:11 +0100

    Add notification status handler.

--- src/main/java/org/springframework/cloud/aws/sample/MvcConfiguration.java
+++ src/main/java/org/springframework/cloud/aws/sample/MvcConfiguration.java
@@ -1,7 +1,9 @@
 package org.springframework.cloud.aws.sample;
 
+import com.amazonaws.services.sns.AmazonSNS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.aws.messaging.endpoint.NotificationMessageHandlerMethodArgumentResolver;
+import org.springframework.cloud.aws.messaging.endpoint.NotificationStatusHandlerMethodArgumentResolver;
 import org.springframework.cloud.aws.messaging.endpoint.NotificationSubjectHandlerMethodArgumentResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
@@ -13,8 +15,9 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 public class MvcConfiguration {
 
     @Autowired
-    public MvcConfiguration(RequestMappingHandlerAdapter requestMappingHandlerAdapter) {
+    public MvcConfiguration(RequestMappingHandlerAdapter requestMappingHandlerAdapter, AmazonSNS amazonSNS) {
         requestMappingHandlerAdapter.getCustomArgumentResolvers().add(new NotificationMessageHandlerMethodArgumentResolver());
         requestMappingHandlerAdapter.getCustomArgumentResolvers().add(new NotificationSubjectHandlerMethodArgumentResolver());
+        requestMappingHandlerAdapter.getCustomArgumentResolvers().add(new NotificationStatusHandlerMethodArgumentResolver(amazonSNS));
     }
 }
